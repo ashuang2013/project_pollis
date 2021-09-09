51e399219a250d03306702f869e01a6351b85d3c
WoodyWoodward <lewoodward1130@gmail.com>
2021-09-08 20:44:28 -0400

    Fixing pull

--- StoryDriver.java
+++
@@ -1,9 +0,0 @@
-public class StoryDriver
-{
-   public static void main(String[] arg)
-   {
-   QuadNode parent = new QuadNode();
-   QuadTree storyline = new QuadTree(parent);
-   storyline.createNewSet();
-   }
-}
